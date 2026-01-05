619ee19dd02ac88c09f8088f498844caabb5225b
sanjay.R <nr.sanjay848@gmail.com>
2026-01-05 14:33:47 +0530

    jj

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/Crse-project/',
+   
   plugins: [react()],
+  base: '/Crse-project/',
 })
